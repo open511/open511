6540ba8d85549a425821c2554f22edc52d5f737c
Michael Mulley <michael@michaelmulley.com>
2013-12-04 16:21:30 -0500

    Move to GMLv3

--- open511_validator/converter.py
+++ open511_validator/converter.py
@@ -191,32 +191,29 @@ def json_link_to_xml(val, rel='related'):
 # GEOSPATIAL
 ##########
 
+def _reverse_geojson_coords(coords):
+    return "%s %s" % (coords[1], coords[0])
+
 def geojson_to_gml(gj, set_srs=True):
     """Given a dict deserialized from a GeoJSON object, returns an lxml Element
     of the corresponding GML geometry."""
     tag = G(gj['type'])
     if set_srs:
-        tag.set('srsName', 'EPSG:4326')
+        tag.set('srsName', 'urn:ogc:def:crs:EPSG::4326')
 
     if gj['type'] == 'Point':
-        tag.append(G.coordinates(','.join(str(c) for c in gj['coordinates'])))
+        tag.append(G.pos(_reverse_geojson_coords(gj['coordinates'])))
     elif gj['type'] == 'LineString':
-        tag.append(G.coordinates(' '.join(
-            ','.join(str(c) for c in ll)
-            for ll in gj['coordinates']
-        )))
+        tag.append(G.posList(' '.join(_reverse_geojson_coords(ll) for ll in gj['coordinates'])))
     elif gj['type'] == 'Polygon':
         rings = [
             G.LinearRing(
-                G.coordinates(' '.join(
-                    ','.join(str(c) for c in ll)
-                    for ll in ring
-                ))
+                G.posList(' '.join(_reverse_geojson_coords(ll) for ll in ring))
             ) for ring in gj['coordinates']
         ]
-        tag.append(G.outerBoundaryIs(rings.pop(0)))
+        tag.append(G.exterior(rings.pop(0)))
         for ring in rings:
-            tag.append(G.innerBoundaryIs(ring))
+            tag.append(G.interior(ring))
     elif gj['type'] in ('MultiPoint', 'MultiLineString', 'MultiPolygon'):
         single_type = gj['type'][5:]
         member_tag = single_type[0].lower() + single_type[1:] + 'Member'
@@ -229,8 +226,47 @@ def geojson_to_gml(gj, set_srs=True):
 
     return tag
 
+def _reverse_gml_coords(s):
+    coords = s.split(' ')
+    gj = []
+    for i in xrange(0, len(coords), 2):
+        gj.append((float(coords[i+1]), float(coords[i])))
+    return gj
+
 def gml_to_geojson(el):
     """Given an lxml Element of a GML geometry, returns a dict in GeoJSON format."""
+    if el.get('srsName') != 'urn:ogc:def:crs:EPSG::4326':
+        if el.get('srsName') == 'EPSG:4326':
+            return _gmlv2_to_geojson(el)
+        else:
+            raise NotImplementedError("Unrecognized srsName %s" % el.get('srsName'))
+    tag = el.tag.replace('{%s}' % NS_GML, '')
+    if tag == 'Point':
+        coordinates = _reverse_gml_coords(el.findtext('{%s}pos' % NS_GML))[0]
+    elif tag == 'LineString':
+        coordinates = _reverse_gml_coords(el.findtext('{%s}posList' % NS_GML))
+    elif tag == 'Polygon':
+        coordinates = []
+        for ring in el.xpath('gml:exterior/gml:LinearRing/gml:posList', namespaces=NSMAP) \
+                + el.xpath('gml:interior/gml:LinearRing/gml:posList', namespaces=NSMAP):
+            coordinates.append(_reverse_gml_coords(ring.text))
+    elif tag in ('MultiPoint', 'MultiLineString', 'MultiPolygon'):
+        single_type = tag[5:]
+        member_tag = single_type[0].lower() + single_type[1:] + 'Member'
+        coordinates = [
+            gml_to_geojson(member)['coordinates']
+            for member in el.xpath('gml:%s/gml:%s' % (member_tag, single_type), namespaces=NSMAP)
+        ]
+    else:
+        raise NotImplementedError
+
+    return {
+        'type': tag,
+        'coordinates': coordinates
+    }
+
+def _gmlv2_to_geojson(el):
+    """Translates a deprecated GML 2.0 geometry to GeoJSON"""
     tag = el.tag.replace('{%s}' % NS_GML, '')
     if tag == 'Point':
         coordinates = [float(c) for c in el.findtext('{%s}coordinates' % NS_GML).split(',')]
