83f0fa4ab56bfc81e36984676926387bc6bd4185
Michael Mulley <michael@michaelmulley.com>
2015-11-17 18:31:54 -0500

    Add functions to validate a single item e.g. one <event> rather than a whole document

--- open511/validator/__init__.py
+++ open511/validator/__init__.py
@@ -1,6 +1,16 @@
+try:
+    unicode
+except NameError:
+    unicode = str
+
+from copy import deepcopy
 import os
 
 from lxml import etree, isoschematron
+
+from open511.converter import pluralize
+from open511.converter.o5xml import json_struct_to_xml
+from open511.utils.serialization import get_base_open511_element
 
 class Open511ValidationError(Exception):
     pass
@@ -11,6 +21,8 @@ SCHEMATRON_PATH = os.path.join(_schema_dir, 'open511.schematron')
 
 RELAXNG_LXML = etree.RelaxNG(etree.parse(RELAXNG_PATH))
 SCHEMATRON_LXML = isoschematron.Schematron(etree.parse(SCHEMATRON_PATH))
+
+DEFAULT_VERSION = 'v1'
 
 def validate(doc):
     errors = []
@@ -26,3 +38,29 @@ def validate(doc):
     if errors:
         raise Open511ValidationError("\n\n".join(errors))
     return True
+
+def _make_link(rel, href):
+    l = etree.Element('link')
+    l.set('rel', rel)
+    l.set('href', href)
+    return l
+
+def validate_single_item(el, version=DEFAULT_VERSION, ignore_missing_urls=False):
+    doc = get_base_open511_element(version=version)
+
+    if ignore_missing_urls:
+        el = deepcopy(el)
+        if not el.xpath('link[rel=self]'):
+            el.append(_make_link('self', '/fake/data'))
+        if not el.xpath('link[rel=jurisdiction]'):
+            el.append(_make_link('jurisdiction', 'http://example.com/fake/jurisdiction'))
+
+    container = etree.Element(pluralize(el.tag))
+    container.append(el)
+    doc.append(container)
+    return validate(doc)
+
+def validate_single_json_item(obj, resource_type='event',
+        ignore_missing_urls=False, version=DEFAULT_VERSION):
+    return validate_single_item(json_struct_to_xml(obj, root=resource_type, custom_namespace='custom'),
+        version=version, ignore_missing_urls=ignore_missing_urls)
